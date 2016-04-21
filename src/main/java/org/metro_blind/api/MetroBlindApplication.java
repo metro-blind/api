58960adadea01aa9babd02679718cb0b0f4e8a8f
Alex ARNAUD <arnaud_a@etna-alternance.net>
2016-04-21 21:45:34 +0200

    eneable CORS in DropWizard

--- src/main/java/org/metro_blind/api/MetroBlindApplication.java
+++ src/main/java/org/metro_blind/api/MetroBlindApplication.java
@@ -9,9 +9,24 @@ import io.dropwizard.hibernate.HibernateBundle;
 import org.metro_blind.api.model.*;
 import org.metro_blind.api.resources.*;
 
+import java.util.EnumSet;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration.Dynamic;
+
 public class MetroBlindApplication extends Application<MetroBlindConfiguration> {
     public static void main(String[] args) throws Exception {
 	new MetroBlindApplication().run(args);
+    }
+
+    private void configureCors(Environment environment) {
+	Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+	filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+	filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
+	filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+	filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+	filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+	filter.setInitParameter("allowCredentials", "true");
     }
 
     private final HibernateBundle<MetroBlindConfiguration> hibernateBundle =
@@ -35,6 +50,7 @@ public class MetroBlindApplication extends Application<MetroBlindConfiguration> 
     @Override
     public void run(MetroBlindConfiguration config,
 		    Environment environment) throws ClassNotFoundException {
+	this.configureCors(environment);
 	final StationDAO stationDao = new StationDAO(hibernateBundle.getSessionFactory());
 	environment.jersey().register(new StationResource(stationDao));
 	final ItineraryDAO itineraryDao = new ItineraryDAO(hibernateBundle.getSessionFactory());
